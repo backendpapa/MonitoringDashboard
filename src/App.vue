85025ffe06601701567a096ec59c70239e39bea7
Akeju Paul <akejupaul10@gmail.com>
2021-07-19 10:26:00 +0100

    make it better

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <v-app-bar
       flat
       dense
+      class="nav"
       color="white"
       height="20vh"
       
@@ -13,7 +14,7 @@
     <!-- Navigation Drawer -->
     <v-row class="main-row"  no-gutters>
       <v-col  cols="2">
-        Side Bar 
+        <p class="text-h3 font-weight-bold">Side Bar </p>
       </v-col>
       <v-col cols="10" class="rounded-left">
 
@@ -42,15 +43,20 @@ export default {
 };
 </script>
 <style>
+.nav{
+  font-family: 'Source Sans Pro', sans-serif;
+}
 .rounded-left{
   border-top-left-radius: 15px;
   overflow-y: scroll;
   height: 100%;
   background-color: #ECF4F7;
+ 
 
 }
 .main-row{
-  height:80vh
+  height:80vh;
+  font-family: 'Source Sans Pro', sans-serif;
 }
 ::-webkit-scrollbar {
     display: none;
