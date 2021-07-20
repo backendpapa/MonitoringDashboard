94dd83fa0ea551ee332d257a3e3b5d89e32f42cd
Akeju Paul <akejupaul10@gmail.com>
2021-07-20 02:06:38 +0100

    make it better

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,73 @@
 <template>
 <div>
-  Main Area
+  <p class="font-weight-bold">Daily Monitoring</p>
+  <v-row >
+    <v-col cols="3">
+      <v-card flat>
+        <v-row no-gutters class="pa-2" align="center" justify="center">
+          <v-col>
+            <p class="text-center"><img width="60" src="../assets/straw.png"/></p>
+          </v-col>
+          <v-col>
+            <p class="text-caption font-weight-bold">Calories</p>
+            <p class="text-h5">2814</p>
+            <p class="grey--text font-weight-bold text-caption" style="margin-top:-14px">DAILY AVERAGE</p>
+            
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+
+  <!-- Second Column -->
+  <v-col cols="3">
+      <v-card flat>
+        <v-row no-gutters class="pa-2" align="center" justify="center">
+          <v-col>
+            <p class="text-center"><img width="60" src="../assets/straw.png"/></p>
+          </v-col>
+          <v-col>
+            <p class="text-caption font-weight-bold">Calories</p>
+            <p class="text-h5">2814</p>
+            <p class="grey--text font-weight-bold text-caption" style="margin-top:-14px">DAILY AVERAGE</p>
+            
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+
+    <!-- Third column -->
+    <v-col cols="3">
+      <v-card flat>
+        <v-row no-gutters class="pa-2" align="center" justify="center">
+          <v-col>
+            <p class="text-center"><img width="60" src="../assets/straw.png"/></p>
+          </v-col>
+          <v-col>
+            <p class="text-caption font-weight-bold">Calories</p>
+            <p class="text-h5">2814</p>
+            <p class="grey--text font-weight-bold text-caption" style="margin-top:-14px">DAILY AVERAGE</p>
+            
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+    <!-- Last column -->
+    <v-col cols="3">
+      <v-card flat>
+        <v-row no-gutters class="pa-2" align="center" justify="center">
+          <v-col>
+            <p class="text-center"><img width="60" src="../assets/straw.png"/></p>
+          </v-col>
+          <v-col>
+            <p class="text-caption font-weight-bold">Calories</p>
+            <p class="text-h5">2814</p>
+            <p class="grey--text font-weight-bold text-caption" style="margin-top:-14px">DAILY AVERAGE</p>
+            
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+  </v-row>
 </div>
 </template>
 
